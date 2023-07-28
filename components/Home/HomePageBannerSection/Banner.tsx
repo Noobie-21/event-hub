350a30a10af65153c7028409873145cf38a02534
noobie <amangupta807646@gmail.com>
2023-07-28 16:18:59 +0530

    commit

--- components/Home/HomePageBannerSection/Banner.tsx
+++ components/Home/HomePageBannerSection/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = (props: Props) => {
     <Flex className="w-full h-[90vh] justify-between items-center p-6 bg-slate-300">
       <Flex className="w-full h-full items-center text-[2.5rem] text-violet-400 px-4">
         <Text className=" w-5/6 rems ">
-          “<span className="text-[3rem] text-red-500">Luck</span> is a matter of
+          “<span className="text-[3rem] text-red-400">Luck</span> is a matter of
           preparation <span className="text-pink-400">meeting opportunity</span>
           .”{" "}
           <span className="text-sm text-red-500">- Lucius Annaeus Seneca</span>
