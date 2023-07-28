fd5a45e6793c7b7d8443adac74d7d83c5ea3d39e
noobie <amangupta807646@gmail.com>
2023-07-28 15:19:59 +0530

    some update in provider file

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,12 +3,11 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "@/chakras/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider cssVarsRoot={() => {}}>{children}</ChakraProvider>
+      <ChakraProvider>{children}</ChakraProvider>
     </CacheProvider>
   );
 }
